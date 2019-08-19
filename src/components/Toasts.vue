830b7211d982ef0def84fc42f2ca021062e1f873
Yuriy Potemkin <ysoft2000@mail.ru>
2019-08-19 21:02:44 +0300

    show icon

--- src/components/Toasts.vue
+++ src/components/Toasts.vue
@@ -15,7 +15,7 @@
           <div :class="'bg-' + item.type" :style="{ width: getCurrentProgress(item) + '%' }" />
         </div>
 
-        <div class="toast-header" v-if="this.showIcon" :class="'bg-' + item.type" style="height: 35px;">
+        <div class="toast-header" v-if="item.showIcon" :class="'bg-' + item.type" style="height: 35px;">
           <strong class="ml-auto">
             <i class="toast-icon" :class="getIconClass(item)" />
           </strong>
